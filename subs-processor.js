4a7b596f82486de2811d4e8c7fbceebe65b3f934
C9E4F7 <c9e4f7@outlook.com>
2019-03-27 23:53:27 +0200

    Spaces to tabs

--- subs-processor.js
+++ subs-processor.js
@@ -16,98 +16,98 @@ const genToken = uuidv4();
 
 http.createServer(function (req, res) {
 
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, Authorization, Accept');
-  res.setHeader('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
-  res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS');
-  res.setHeader('access-control-allow-origin', corsVar);
-  res.setHeader('Access-Control-Max-Age', '1728000');
-  res.setHeader('Connection', 'keep-alive');
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.setHeader('Access-Control-Allow-Headers', 'Origin, Authorization, Accept');
+	res.setHeader('Access-Control-Allow-Headers', 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range');
+	res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS');
+	res.setHeader('access-control-allow-origin', corsVar);
+	res.setHeader('Access-Control-Max-Age', '1728000');
+	res.setHeader('Connection', 'keep-alive');
 
-  // sending progress to user
-  if (req.url.match(/\/getProgressByToken.*/)) {
-    res.end(JSON.stringify(cmds.processResponse));
-  };
+	// sending progress to user
+	if (req.url.match(/\/getProgressByToken.*/)) {
+		res.end(JSON.stringify(cmds.processResponse));
+	};
 
-  if (req.url == '/uploadSubtitle' && !reqhappened) {
+	if (req.url == '/uploadSubtitle' && !reqhappened) {
 
-    if (req.method === 'OPTIONS'){
-      res.statusCode = 204;
-      res.end();
+		if (req.method === 'OPTIONS'){
+			res.statusCode = 204;
+			res.end();
 
-    } else {
+		} else {
 
-      res.statusCode = 200;
+			res.statusCode = 200;
 
-      reqhappened = true;
-      var form = new formidable.IncomingForm();
+			reqhappened = true;
+			var form = new formidable.IncomingForm();
 
-      //Sane Form options
-      form.maxFields = 1
-      form.encoding = 'utf-8';
-      form.maxFileSize = '1024000';
+			//Sane Form options
+			form.maxFields = 1
+			form.encoding = 'utf-8';
+			form.maxFileSize = '1024000';
 
-      form.parse(req, function (err, fields, files) {
-      });
+			form.parse(req, function (err, fields, files) {
+			});
 
-      // file is moved to upload folder and renamed to uuid
-      form.on('fileBegin', function (name, file){
-        file.path = "./upload/" + genToken;
-      });
+			// file is moved to upload folder and renamed to uuid
+			form.on('fileBegin', function (name, file){
+				file.path = "./upload/" + genToken;
+			});
 
-      form.on('file', function (name, file) {
+			form.on('file', function (name, file) {
 
-        //frontend needs to know if upload was successful and receive the token
-        var successResponse = { success: "", token: ""};
-        var fileText = fs.readFileSync(file.path).toString('utf-8');
+				//frontend needs to know if upload was successful and receive the token
+				var successResponse = { success: "", token: ""};
+				var fileText = fs.readFileSync(file.path).toString('utf-8');
 
-        try {
-          const parsedFile = webvtt.parse(fileText);
+				try {
+					const parsedFile = webvtt.parse(fileText);
 
-          if (parsedFile.valid == true) {
+					if (parsedFile.valid == true) {
 
-            // if file is valid, success is true and provide token
-            successResponse.success = "true";
-            successResponse.token = genToken;
-            res.end(JSON.stringify(successResponse));
+						// if file is valid, success is true and provide token
+						successResponse.success = "true";
+						successResponse.token = genToken;
+						res.end(JSON.stringify(successResponse));
 
-            // upload file to ipfs
-            cmds.ipfs_cmds.ipfsUpload(file.path, "ipfsAddSource");
-            cmds.checkIfFinished();
+						// upload file to ipfs
+						cmds.ipfs_cmds.ipfsUpload(file.path, "ipfsAddSource");
+						cmds.checkIfFinished();
 
-          } else {
+					} else {
 
-            // if not valid, success is false, no token, end process
-            successResponse.success = "false";
-            res.end(JSON.stringify(successResponse));
-            process.exit();
-          }
+						// if not valid, success is false, no token, end process
+						successResponse.success = "false";
+						res.end(JSON.stringify(successResponse));
+						process.exit();
+					}
 
 
-        }
-        catch(err) {
-          // if error, success is false, no token, end process
-          console.log(err);
-          successResponse.success = "false";
-          res.end(JSON.stringify(successResponse));
-          process.exit();
-        }
+				}
+				catch(err) {
+					// if error, success is false, no token, end process
+					console.log(err);
+					successResponse.success = "false";
+					res.end(JSON.stringify(successResponse));
+					process.exit();
+				}
 
-      });
+			});
 
-      form.on('error', function(err) {
-  console.error('Error', err)
-  process.exit();
-  		});
+			form.on('error', function(err) {
+	console.error('Error', err)
+	process.exit();
+			});
 
-    }
+		}
 
-  } else {
-    res.end("There's nothing here for you");
-  }
+	} else {
+		res.end("There's nothing here for you");
+	}
 
 }).listen(5000, ()=> {
 
-  console.log("listening on port 5000");
+	console.log("listening on port 5000");
 
 });
